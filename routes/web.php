c7709668a4ba2b0698c5ede539b9cb3ed4519e46
Bilal_Al_Laham <bilalallaham75@gmail.com>
2024-10-24 19:26:31 +0300

    updating database

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
